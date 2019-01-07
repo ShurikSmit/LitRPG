ff158d9808ccd7c0404de9f25422201713fab1a2
ShurikSmit <pr.shurya@gmail.com>
2019-01-07 19:58:45 +0200

    Добавил задания в main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
 
+import com.company.engine.EngineMain;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //запуск главного экрана (нужен класс который Создавать шаблоны форм с экранами, замутить переключение между экранами, все дела)
+        //настройки (ну будет файлик конфиг с которого будем тянуть параметры игры, размер экрана и т.д.)
+        //создание (class CreateCharacter) или загрузка персонажа (class LoadSaveCharacter)
+        //запуск игры (class EngineMain схема игрового процесса)
+        EngineMain engineMain = new EngineMain();
+        engineMain.startGame();
     }
 }
