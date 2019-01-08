bb663421b845728210bffa3f33f97ad5184ee40c
ShurikSmit <pr.shurya@gmail.com>
2019-01-08 18:53:01 +0200

    сделал измение характеристит +1 и -1

--- src/com/company/engine/crtCharTest.java
+++ src/com/company/engine/crtCharTest.java
@@ -3,6 +3,9 @@ package com.company.engine;
 public class crtCharTest {
     public static void main(String[] args) throws Exception {
         CreateCharacter createCharacter = new CreateCharacter();
+
+        createCharacter.changeStats(1, "CharSTR");
+        System.out.println(createCharacter.getCharSTR());
         createCharacter.CreateCharFile();
     }
 }
