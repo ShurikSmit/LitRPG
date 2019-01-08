973f83c2a0058f63ace52accf8c01fb9b9b95030
ShurikSmit <pr.shurya@gmail.com>
2019-01-08 18:53:46 +0200

    сделал измение характеристит +1 и -1

---
+++ src/com/company/engine/CreateCharacter.java
@@ -0,0 +1,61 @@
+package com.company.engine;
+import java.io.*;
+import java.lang.*;
+public class CreateCharacter {
+    private static int CharSTR = 0;
+    private static int CharAGIL = 0;
+    private static int CharINT = 0;
+    private static String CharName = "Рикардо Милос";
+    public void setCharSTR(int newCharSTR){
+        CharSTR+=newCharSTR;
+    }
+    public int getCharSTR(){
+        return CharSTR;
+    }
+    public void setCharAGIL(int newCharAGIL){
+        CharAGIL+=newCharAGIL;
+    }
+    public int getCharAGIL(){
+        return CharAGIL;
+    }
+    public void setCharINT(int newCharINT){
+        CharINT+= newCharINT;
+    }
+    public int getCharINT(){
+        return CharINT;
+    }
+    public void setCharName(String newCharName){
+        CharName = newCharName;
+    }
+    public String getCharName(){
+        return CharName;
+    }
+
+    public void CreateCharFile () throws IOException {
+        FileWriter fr = new FileWriter("CharFile.txt");
+        String text = "";
+        text+= "NAME:"+getCharName()+"\n";
+        text+= "AGIL:"+getCharAGIL()+"\n";
+        text+= "INT:"+getCharINT()+"\n";
+        text+= "STR:"+getCharSTR()+"\n";
+        fr.write(text);
+        fr.close();
+    }
+    //если 1 стат плюсуется, 0 отнимается
+    public void changeStats(int type, String StatsName){
+        int number;
+        if (type == 0){
+            number=-1;
+        }else{
+            number=1;
+        }
+        switch (StatsName){
+            case "CharSTR": setCharSTR(number);
+                 break;
+            case "CharAGIL":setCharAGIL(number);
+                break;
+            case "CharINT":setCharINT(number);
+                break;
+        }
+    }
+}
