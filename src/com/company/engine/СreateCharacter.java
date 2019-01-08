849429d76a0ddd16d80921fd5c30d6ec2d4a13f5
ShurikSmit <pr.shurya@gmail.com>
2019-01-08 16:56:50 +0200

    начал писать createChar

--- src/com/company/engine/СreateCharacter.java
+++ src/com/company/engine/СreateCharacter.java
@@ -1,5 +1,28 @@
 package com.company.engine;
 
 public class СreateCharacter {
-    
+    private static int CharSTR = 0;
+    private static int CharAGIL = 0;
+    private static int CharINT = 0;
+    private static String CharName = "Рикардо Миллос";
+    public void setCharSTR(int newCharSTR){
+        CharSTR+=newCharSTR;
+    }
+    public int getCharSTR(){
+        return CharSTR;
+    }
+    public void setCharAGIL(int newCharAGIL){
+        CharAGIL+=newCharAGIL;
+    }
+    public int getCharAGIL(){
+        return CharAGIL;
+    }
+    public void setCharINT(int newCharINT){
+        CharINT+=newCharINT;
+    }
+    public int getCharINT(){
+        return CharINT;
+    }
+
+
 }
