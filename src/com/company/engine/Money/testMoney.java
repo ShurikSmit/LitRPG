9261b171f24468b42cf84aa3821a0b7840146070
Asdef1702 <asdef333@gmail.com>
2019-01-13 23:54:57 +0200

    Merge branch 'master' of C:\Users\asdef\IdeaProjects\LitRPG with conflicts.

--- src/com/company/engine/Money/testMoney.java
+++ src/com/company/engine/Money/testMoney.java
@@ -5,11 +5,9 @@ public class testMoney {
         //Money money = new Money();
         Money money = new Money(1,91,99);
         money.sumMoney();
-        money.changeMoney(true,1,2,999 );
+        money.changeMoney(true,0,0,999 );
         money.sumMoney();
-        money.changeMoney(false,1,2,999 );
-        money.sumMoney();
-//        PlayerMoney playerMoney = new PlayerMoney();
-//        playerMoney.sumMoney();
+        PlayerMoney playerMoney = new PlayerMoney();
+        PlayerMoney.sumMoney();
     }
 }
