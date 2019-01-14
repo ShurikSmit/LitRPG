a29993d9cf6cfc343db8b861994299f71cd82504
GoldenUA <vl0951198320ad@gamil.com>
2019-01-14 19:49:43 +0200

    ...

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         //запуск главного экрана (нужен класс который Создавать шаблоны форм с экранами, замутить переключение между экранами, все дела)
         //настройки (ну будет файлик конфиг с которого будем тянуть параметры игры, размер экрана и т.д.)
         //создание (class CreateCharacter) или загрузка персонажа (class LoadSaveCharacter)
